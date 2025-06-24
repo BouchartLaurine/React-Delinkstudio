d0c7b69c7fef6ab14292341562505c6f95efd4c6
Laurine <bouchartlaurine7@gmail.com>
2025-06-24 14:27:30 +0200

    Nav - add hoover + class active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,20 @@
-import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className='bg-header' >
+      <div className='bg-header'>
         <h1>Del'Ink Studio</h1>
         <h2>Vos histoires, gravées à l'encre noire</h2>
       </div>
-      <div className="navbar-logo"></div>
       <ul className="navbar-links">
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/galerie">Galerie</Link></li>
-        <li><Link to="/form">Prise de rendez-vous</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>Accueil</NavLink></li>
+        <li><NavLink to="/galerie" className={({ isActive }) => isActive ? 'active' : ''}>Galerie</NavLink></li>
+        <li><NavLink to="/form" className={({ isActive }) => isActive ? 'active' : ''}>Prise de rendez-vous</NavLink></li>
+        <li><NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>Contact</NavLink></li>
       </ul>
     </nav>
   );
 };
-
-
 
 export default Navbar;
