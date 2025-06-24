047781210ccb92bc10e94a1a7a33486da9725799
Laurine <bouchartlaurine7@gmail.com>
2025-06-24 10:24:51 +0200

    add pages galery + style

--- src/page/Galerie.jsx
+++ src/page/Galerie.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
 
 const Galerie = () => {
   return (
-    <div>
-      <h1>Galerie En cours de développement</h1>
-    </div>
+    <main>
+      <h1>Découvrez une sélection de créations uniques, réalisées avec soin dans un style réaliste noir et gris, inspiré par la nature et les émotions.</h1>
+     <div className="galerie-container">
+       <div className="galerie">
+         <ul className="galerie-list">
+         <li className="image-wrapper">
+           <img src="./tatoo-portrait.jpg" alt="tatouage portrait" />
+          </li>
+          <li className="image-wrapper">
+           <img src="./tatoo-fleurs.jpg" alt="tatouage de fleurs" />
+          </li>
+          <li className="image-wrapper">
+           <img src="./tatoo-leopard.jpg" alt="tatouage léopard" />
+          </li>
+          <li className="image-wrapper">
+           <img src="./tatoo-fleurs2.jpg" alt="tatouage de fleurs" />
+          </li>
+        </ul>
+       </div>
+
+        <div className="galerie2">
+        <img src="./tatoo-.jpg" alt="" />
+        </div>
+      </div>
+
+    </main>
   );
 };
 
